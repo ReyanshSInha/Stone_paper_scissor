9526215fe443d25da20053ae2a30a7617baee3ee
Reyansh Sinha <reyanshsinha19@gmail.com>
2022-07-29 15:40:46 +0530

    rock paper scissor app with ui

--- script.js
+++ script.js
@@ -2,65 +2,126 @@
 
 function computerPlay(){
     let randomChoice = Math.floor(Math.random()*3 + 1)
-    
+    const displayComputerChoice = document.querySelector('.computer-selection')    
     switch (randomChoice){
         case 1:
-            return "Stone"
+            
+            displayComputerChoice.textContent = 'rock'
+
+            return "rock"
             break;
         
         case 2:
-            return "Paper"
+             
+            displayComputerChoice.textContent = 'paper'
+
+            return "paper"
             break;
 
         case 3:
-            return "Scissor"
+             
+            displayComputerChoice.textContent = 'scissor'
+
+            return "scissor"
             break;
     }
 
 }
 
 let score = 0;
-
+let compScore = 0;
+let rounds = 0;
 
 function playGround(playerSelection, computerSelection){
-    if(playerSelection.toLowerCase() == "rock" && computerSelection == "paper"){
-        console.log("You Lose! Paper beats Rock")
-    }else if(playerSelection.toLowerCase() == "paper" && computerSelection == "scissor"){
-        console.log("You Lose! Scissor beats Paper")
-    }else if(playerSelection.toLowerCase() == "scissor" && computerSelection == "rock"){
-        console.log("You Lose! Rock beats Scissor")
-    }else if(playerSelection.toLowerCase() == "rock" && computerSelection == "scissor"){
-        console.log("You win! Rock beats Scissor")
+
+    const scoreBoard = document.querySelector('.score')
+    const scoreNumber = document.querySelector('#scoreNum')
+    const compScoreNumber = document.querySelector("#compScoreNum")
+
+    if(playerSelection == "rock" && computerSelection == "paper"){
+        scoreBoard.textContent = "You Lose! Paper beats Rock"
+        
+        compScore++
+        compScoreNumber.textContent = compScore
+        scoreNumber.textContent = `${score}/${rounds}`
+    }else if(playerSelection == "paper" && computerSelection == "scissor"){
+        scoreBoard.textContent = "You Lose! Scissor beats Paper"
+        
+        compScore++
+        compScoreNumber.textContent = compScore
+        scoreNumber.textContent = `${score}/${rounds}`
+    }else if(playerSelection == "scissor" && computerSelection == "rock"){
+        scoreBoard.textContent = "You Lose! Rock beats Scissor"
+        
+        compScore++
+        compScoreNumber.textContent = compScore
+        scoreNumber.textContent = `${score}/${rounds}`
+    }else if(playerSelection == "rock" && computerSelection == "scissor"){
+        scoreBoard.textContent = "You win! Rock beats Scissor"
+        compScoreNumber.textContent = compScore
         score++
-    }else if(playerSelection.toLowerCase() == "paper" && computerSelection == "rock"){
-        console.log("You win! Paper beats Rock")
+        scoreNumber.textContent = `${score}/${rounds}`
+    }else if(playerSelection == "paper" && computerSelection == "rock"){
+        scoreBoard.textContent = "You win! Paper beats Rock"
+        compScoreNumber.textContent = compScore
         score++
-    }else if(playerSelection.toLowerCase() == "scissor" && computerSelection == "paper"){
-        console.log("You win! Scissor beats Paper")
+        scoreNumber.textContent = `${score}/${rounds}`
+    }else if(playerSelection == "scissor" && computerSelection == "paper"){
+        scoreBoard.textContent = "You win! Scissor beats Paper"
+        compScoreNumber.textContent = compScore
         score++
+        scoreNumber.textContent = `${score}/${rounds}`
+
     }else(
-        console.log("Nobody wins! It's a tie!!!")
+        scoreBoard.textContent = "Nobody wins! It's a tie!!!"
+        
     )
 }
 
+const rock = document.querySelector('#rock')
 
-function game(){
+rock.addEventListener('click', playerChoiceRock)
 
-    for(let i = 0; i < 5; i++){
-        let playerSelection = prompt("what do you choose? ");
-        let computerSelection = computerPlay().toLowerCase();
+function playerChoiceRock(){
+    rounds++
+    const displayChoice = document.querySelector('.player-selection')
 
-        playGround(playerSelection, computerSelection);
-
-        
-    }
-
+    displayChoice.textContent = 'rock'
+    computerPlay()
+    
+    playGround("rock", computerPlay())
+    
 }
 
-game()
+const paper = document.querySelector('#paper')
 
-if(score >= 3){
-    console.log(`You won the match! you scored: ${score}/5 `)
+paper.addEventListener('click', playerChoicePaper)
+
+function playerChoicePaper(){
+    rounds++
+    const displayChoice = document.querySelector('.player-selection')
+
+    displayChoice.textContent = 'paper'
+    computerPlay()
+    playGround("paper", computerPlay())
+}
+
+const scissor = document.querySelector('#scissor')
+
+scissor.addEventListener('click', playerChoiceScissor)
+
+function playerChoiceScissor(){
+    rounds++
+    const displayChoice = document.querySelector('.player-selection')
+
+    displayChoice.textContent = 'scissor'
+    computerPlay()
+    playGround("scissor", computerPlay())
+}
+
+
+if(score >= 3){ 
+    console.log(`You won the match! you scored: ${score}/5 `) 
 }else{
     console.log(`You Lost the match! you scored: ${score}/5`)
 }
